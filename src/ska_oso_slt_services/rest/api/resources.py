c5d904580bf9f12d86603d9a01731f09daac876a
Dayanand Arade <dayanand_arade@persistent.com>
2024-06-17 17:14:18 +0530

    NAK-959 added helm chart support

---
+++ src/ska_oso_slt_services/rest/api/resources.py
@@ -0,0 +1,20 @@
+"""
+Functions which the HTTP requests to individual resources are mapped to.
+
+See the operationId fields of the Open API spec for the specific mappings.
+"""
+
+
+from http import HTTPStatus
+
+
+def get_slts(**kwargs) -> dict:
+    """This function takes query parameters and SLT data source objects
+      to generate a response containing matching SLT data.
+
+    :param query_params (QueryParams): The query parameters.
+    :param tm_data_sources (list): A list of SLT data source objects.
+
+    :returns dict: A dictionary with SLT data satisfying the query.
+    """
+    return {"test":"test"}
