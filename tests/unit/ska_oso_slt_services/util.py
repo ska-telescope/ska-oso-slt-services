e1cc1b5b511a694b7cbc6ebb8edc287fc8fee1a5
Omprakash Verma <omprakash_verma@persistent.com>
2024-08-12 18:46:37 +0530

    NAK-997 Updated workaround

--- tests/unit/ska_oso_slt_services/util.py
+++ tests/unit/ska_oso_slt_services/util.py
@@ -5,7 +5,7 @@ Utility functions to be used in tests
 import json
 import os
 
-# from deepdiff import DeepDiff
+from deepdiff import DeepDiff
 
 
 def load_string_from_file(filename):
@@ -28,7 +28,7 @@ def assert_json_is_equal(json_a, json_b, exclude_paths=None):
     obj_b = json.loads(json_b)  # converts to list
 
     # Compare the objects using DeepDiff
-    # diff = DeepDiff(obj_a, obj_b, ignore_order=True, exclude_paths=exclude_paths)
+    diff = DeepDiff(obj_a, obj_b, ignore_order=True, exclude_paths=exclude_paths)
 
-    # # Raise an assertion error if there are differences
-    # assert {} == diff, f"JSON not equal: {diff}"
+    # Raise an assertion error if there are differences
+    assert {} == diff, f"JSON not equal: {diff}"
