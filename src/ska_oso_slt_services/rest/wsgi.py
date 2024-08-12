3e2e8f58f8bbd74cbdeda1a2baf90a21221b8bef
Omprakash Verma <omprakash_verma@persistent.com>
2024-08-12 17:39:47 +0530

    NAK-997 Updated workaround

--- src/ska_oso_slt_services/rest/wsgi.py
+++ src/ska_oso_slt_services/rest/wsgi.py
@@ -29,5 +29,5 @@ else:
     # use gunicorn logging level for app and module loggers
     gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.setLevel(gunicorn_logger.level)
-    logger = logging.getLogger("ska_oso_slt_services")
+    logger = logging.getLogger("ska_db_oda")
     logger.setLevel(gunicorn_logger.level)
