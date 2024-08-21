beca77bf0da062db274f6e9333d2c4161e8c4e96
Omprakash Verma <omprakash_verma@persistent.com>
2024-08-21 15:52:57 +0530

    NAK-998 Updated values

--- src/ska_oso_slt_services/rest/wsgi.py
+++ src/ska_oso_slt_services/rest/wsgi.py
@@ -8,6 +8,7 @@ from ska_db_oda.rest.wsgi import UniformLogger  # noqa: F401
 
 from ska_oso_slt_services.data_access.postgres_data_acess import PostgresDataAccess, PostgresConnection
 from ska_oso_slt_services.rest import init_app
+LOGGER = logging.getLogger(__name__)
 
 app = init_app()
 
@@ -45,23 +46,26 @@ def create_oda_slt_table():
             table_exist = cursor.fetchone()["table_existence"]
 
     if table_exist:
-        print("---------------> Table Already present")
+        
+        LOGGER.error("---------------> Table Already present")
 
     if not table_exist:
-        print("---------------> Creating Table")
+        LOGGER.error("---------------> Creating Table")
         with connection_pool.connection() as conn:
             with conn.cursor() as cursor:
                 cursor.execute(query)
                 cursor.execute(table_exist_query)
                 table_exist = cursor.fetchone()["table_existence"]
                 if table_exist:
-                    print("----------> Table created successfully")
+                    LOGGER.error("----------> Table created successfully")
 
 
 if __name__ == "__main__":
+    LOGGER.error("---------------> Creating Table in if")
     create_oda_slt_table()
     app.run(host="0.0.0.0", port=5000)
 else:
+    LOGGER.error("---------------> Creating Table in else")
     create_oda_slt_table()
     # presume being run from gunicorn
     # use gunicorn logging level for app and module loggers
