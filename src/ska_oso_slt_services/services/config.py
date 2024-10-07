253c6f9986bda62193b159cabfcfa2ce5123c9f3
Manish Lalwani <manish_lalwani11@persistent.com>
2024-10-07 15:29:54 +0530

    NAK-1054 Minor change

--- src/ska_oso_slt_services/services/config.py
+++ src/ska_oso_slt_services/services/config.py
@@ -10,5 +10,5 @@ class KafkaConfig:
     AUTO_OFFSET_RESET = os.getenv("AUTO_OFFSET_RESET", "latest")
     CLIEND_ID = os.getenv("CLIEND_ID", socket.gethostname())
     CONSUMER_TOPIC = os.getenv("CONSUMER_TOPIC", "oda-to-slt-topic")
-    PRODUCER_TOPIC = os.getenv("Producer_TOPIC", "slt-to-frontend-topic")
-    TOPIC_POLL_TIME = os.getenv("Producer_TOPIC", "1.0")
+    PRODUCER_TOPIC = os.getenv("PRODUCER_TOPIC", "slt-to-frontend-topic")
+    TOPIC_POLL_TIME = os.getenv("TOPIC_POLL_TIME", "1.0")
